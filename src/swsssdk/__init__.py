bb2f57e953c968964d6e20af11e278e714381904
liuh-80 <liuh@microsoft.com>
2022-09-27 03:24:08 +0000

    Fix code issue

--- src/swsssdk/__init__.py
+++ src/swsssdk/__init__.py
@@ -8,6 +8,13 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.NullHandler())
 
+if ('unittest' not in sys.modules.keys() and
+    'mockredis' not in sys.modules.keys() and
+    'mock' not in sys.modules.keys()):
+    msg = "sonic-py-swsssdk been deprecated, please switch to sonic-swss-common."
+    logger.exception(msg)
+    raise ImportError("sonic-py-swsssdk been deprecated, please switch to sonic-swss-common.")
+
 try:
     from .dbconnector import SonicDBConfig, SonicV2Connector
     from .configdb import ConfigDBConnector, ConfigDBPipeConnector
@@ -16,10 +23,3 @@ except (KeyError, ValueError):
     msg = "Failed to database connector objects -- incorrect database config schema."
     logger.exception(msg)
     raise RuntimeError(msg)
-
-if ('unittest' not in sys.modules.keys() and
-    'mockredis' not in sys.modules.keys() and
-    'mock' not in sys.modules.keys()):
-    msg = "sonic-py-swsssdk been deprecated, please switch to sonic-swss-common."
-    logger.exception(msg)
-    raise ImportError("sonic-py-swsssdk been deprecated, please switch to sonic-swss-common.")
